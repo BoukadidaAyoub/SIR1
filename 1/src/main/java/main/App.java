6ab86677d8db70992763ca6c34571b47ca61d88d
NicolasSellier <sellier.nicolas@etudiant.univ-rennes1.fr>
2016-01-06 17:53:47 +0100

    1st commit

---
+++ 1/src/main/java/main/App.java
@@ -0,0 +1,19 @@
+package main;
+
+/**
+ * Hello world!
+ * Ceci est le TP1. 
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int i=0;
+        i=0;
+        i=0;
+        		
+    }
+    
+    public void testMethode(){
+    	System.out.println("");
+    }
+}
